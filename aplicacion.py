c371a5013087f5ea6ef6cd6be1d4d7446c2a674a
Felipe Vélez Fernández <fevelezf@unal.edu.co>
2023-10-09 15:37:32 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -11,45 +11,39 @@ usuarios_df = pd.read_csv('usuarios.csv')
 data = {'Fecha': [], 'Categoría': [], 'Monto': []}
 df = pd.DataFrame(data)
 
-# Función para registrar un nuevo usuario
 def registrar_usuario(username, password):
-    # Verificar si el usuario ya existe
-    if username in usuarios_df['Usuario'].values:
-        return "El usuario ya existe. Por favor, inicia sesión."
-    
-    # Agregar el nuevo usuario al DataFrame
-    nuevo_usuario = pd.DataFrame({'Usuario': [username], 'Contraseña': [password]})
-    usuarios_df = usuarios_df.append(nuevo_usuario, ignore_index=True)
-    usuarios_df.to_csv('usuarios.csv', index=False)  # Guardar el DataFrame en el archivo CSV
-    
-    return "Registro exitoso. Ahora puedes iniciar sesión."
+    # Lee el archivo CSV de usuarios existente o crea uno si no existe
+    try:
+        usuarios_df = pd.read_csv('usuarios.csv')
+    except FileNotFoundError:
+        usuarios_df = pd.DataFrame(columns=['Username', 'Password'])
+
+    # Verifica si el usuario ya existe
+    if username in usuarios_df['Username'].values:
+        return False, "El usuario ya existe. Por favor, elija otro nombre de usuario."
+
+    # Agrega el nuevo usuario y contraseña al DataFrame
+    usuarios_df = usuarios_df.append({'Username': username, 'Password': password}, ignore_index=True)
+
+    # Guarda el DataFrame actualizado en el archivo CSV
+    usuarios_df.to_csv('usuarios.csv', index=False)
+
+    return True, "Registro exitoso. Ahora puede iniciar sesión."
 
 # ...
 
-# Función para iniciar sesión
-def iniciar_sesion():
-    username = st.text_input("Nombre de Usuario:")
-    password = st.text_input("Contraseña:", type="password")
-    
-    if st.button("Iniciar Sesión"):
-        mensaje = iniciar_sesion(username, password)
-        st.write(mensaje)
+def verificar_credenciales(username, password):
+    # Lee el archivo CSV de usuarios
+    try:
+        usuarios_df = pd.read_csv('usuarios.csv')
+    except FileNotFoundError:
+        return False, "No se encontraron usuarios registrados."
 
-    elif st.button("Registrarse"):
-        mensaje_registro = registrar_usuario(username, password)
-        st.write(mensaje_registro)
-    # Buscar el usuario en el DataFrame
-    user_row = usuarios_df[usuarios_df['Usuario'] == username]
-    
-    if user_row.empty:
-        return "El usuario no existe. Regístrate primero."
-    
-    stored_password = user_row.iloc[0]['Contraseña']
-    
-    if password == stored_password:
-        return "Inicio de sesión exitoso."
+    # Verifica las credenciales
+    if (usuarios_df['Username'] == username).any() and (usuarios_df['Password'] == password).any():
+        return True, "Inicio de sesión exitoso."
     else:
-        return "Contraseña incorrecta. Inténtalo de nuevo."
+        return False, "Credenciales incorrectas. Por favor, verifique su nombre de usuario y contraseña."
     
 
 # Título de la aplicación
@@ -58,19 +52,61 @@ st.title("Seguimiento de Gastos Personales")
 # Menú desplegable en la barra lateral
 menu_option = st.sidebar.selectbox("Menú", ["Inicio", "Registro", "Salir"])
 
-# Opciones del menú
+# Inicio de sesión
 if menu_option == "Inicio":
     st.write("Bienvenido al inicio de la aplicación.")
-elif menu_option == "Registro":
-    # Agregar Gasto
-    fecha = st.text_input("Ingrese la fecha (YYYY-MM-DD):")
-    categoria = st.text_input("Ingrese la categoría del gasto:")
-    monto = st.number_input("Ingrese el monto del gasto:")
 
-    if st.button("Agregar Gasto"):
-        # Agregar el gasto al DataFrame
-        df.loc[len(df)] = [fecha, categoria, monto]
-        st.success("Gasto agregado exitosamente.")
+    # Campos de inicio de sesión
+    username = st.text_input("Nombre de Usuario:")
+    password = st.text_input("Contraseña:", type="password")
+
+    if st.button("Iniciar Sesión"):
+        login_successful, message = verificar_credenciales(username, password)
+        if login_successful:
+            st.success(message)
+            opcion = st.selectbox("Seleccione una opción:", ["Agregar Gasto", "Calcular Estadísticas"])
+            if opcion == "Agregar Gasto":
+                fecha = st.text_input("Ingrese la fecha (YYYY-MM-DD):")
+                categoria = st.text_input("Ingrese la categoría del gasto:")
+                monto = st.number_input("Ingrese el monto del gasto:")
+            elif opcion == "Calcular Estadísticas":
+                if not df.empty:
+                    # Estadísticas generales
+                    promedio_total = df['Monto'].mean()
+                    gasto_total = df['Monto'].sum()
+
+                    # Estadísticas por categoría
+                    estadisticas_por_categoria = df.groupby('Categoría')['Monto'].sum()
+
+                    st.write("\nEstadísticas Generales:")
+                    st.write(f"Promedio Mensual: {promedio_total}")
+                    st.write(f"Gasto Total: {gasto_total}")
+
+                    st.write("\nEstadísticas por Categoría:")
+                    st.write(estadisticas_por_categoria)
+
+                    # Crear un gráfico de pastel de la distribución de gastos por categoría
+                    fig, ax = plt.subplots()
+                    ax.pie(estadisticas_por_categoria, labels=estadisticas_por_categoria.index, autopct='%1.1f%%')
+                    ax.set_title('Distribución de Gastos por Categoría')
+                    st.pyplot(fig)
+                else:
+                    st.warning("No hay datos de gastos para calcular estadísticas.")
+        else:
+            st.error(message)
+elif menu_option == "Registro":
+    st.write("Registro de Usuario")
+
+    # Campos de registro
+    new_username = st.text_input("Nuevo Nombre de Usuario:")
+    new_password = st.text_input("Nueva Contraseña:", type="password")
+
+    if st.button("Registrar"):
+        registration_successful, message = registrar_usuario(new_username, new_password)
+        if registration_successful:
+            st.success(message)
+        else:
+            st.error(message)
 elif menu_option == "Salir":
     st.balloons()
     st.stop()
