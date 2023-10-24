5cad2bca193a97c7157c11667cb702e003c0b4f9
Felipe Vélez Fernández <fevelezf@unal.edu.co>
2023-10-23 20:14:43 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -339,15 +339,13 @@ else:
         aceptar_politica = False  # Inicializa la variable aceptar_politica
 
         # Botón de registro de usuario en la primera columna
-        if col1.button("Registrarse") and aceptar_politica:
-            registration_successful, message = registrar_usuario(new_username, new_password)
-            if registration_successful:
-                st.success(message)
+        if col1.button("Registrarse"):
+            if aceptar_politica:
+                registration_successful, message = registrar_usuario(new_username, new_password, first_name, last_name, email, confirm_password)
+                if registration_successful:
+                    st.success(message)
             else:
-                st.error(message)
-
-        if not aceptar_politica:
-            st.warning("Por favor, acepta la política de datos personales antes de registrarte.")
+                st.warning("Por favor, acepta la política de datos personales antes de registrarte.")
 
         # Botón para abrir la ventana emergente en la segunda columna
         if col2.button("Ver Política de Tratamiento de Datos"):
@@ -355,10 +353,7 @@ else:
                 politica = archivo.read()
                 with st.expander("Política de Tratamiento de Datos"):
                     st.write(politica)
-                    
-
                     aceptar_politica = st.checkbox("Acepta la política de datos personales")
-                    aceptar_politica = True
                 
     elif menu_option == "Salir":
         st.balloons()
