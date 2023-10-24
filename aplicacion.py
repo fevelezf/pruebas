4c7f516a305d99c10efea15bb5bddf70aa960357
Felipe Vélez Fernández <fevelezf@unal.edu.co>
2023-10-23 20:10:39 -0500

    Update aplicacion.py

--- aplicacion.py
+++ aplicacion.py
@@ -335,15 +335,9 @@ else:
 
         # Crear dos columnas para los botones
         col1, col2 = st.columns(2)
-        # Casilla de verificación para aceptar la política de datos personales        
-        # Agrega un botón para mostrar la política de datos
-        if col2.button("Mostrar Política de Datos"):
-            with open("politica_datos.txt", "r") as archivo:
-                politica = archivo.read()
-                with st.expander("Política de Tratamiento de Datos"):
-                    st.write(politica)
-                # Luego de mostrar la política, muestra la casilla de aceptación
-                aceptar_politica = st.checkbox("Acepta la política de datos personales")
+        # Casilla de verificación para aceptar la política de datos personales
+        aceptar_politica = False  # Inicializa la variable aceptar_politica
+
         # Botón de registro de usuario en la primera columna
         if col1.button("Registrarse") and aceptar_politica:
             registration_successful, message = registrar_usuario(new_username, new_password)
@@ -355,8 +349,13 @@ else:
         if not aceptar_politica:
             st.warning("Por favor, acepta la política de datos personales antes de registrarte.")
 
-
-
+        # Botón para abrir la ventana emergente en la segunda columna
+        if col2.button("Ver Política de Tratamiento de Datos"):
+            with open("politica_datos.txt", "r") as archivo:
+                politica = archivo.read()
+                with st.expander("Política de Tratamiento de Datos"):
+                    st.write(politica)
+                aceptar_politica = st.checkbox("Acepta la política de datos personales")
     elif menu_option == "Salir":
         st.balloons()
         st.stop()
