85eca4e33a8ee77ffb6718c23413fb0f44bd50a4
Sebastian Aguinaga Velásquez <saguinagavelasquez@gmail.com>
2023-10-28 11:59:31 -0500

    bug1

--- prueba.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-nombres = ["Juan", "María", "Pedro", "Luisa", "Ana"]
-
-# Crear un diccionario a partir de la lista de nombres con 0 como valor
-diccionario_nombres = dict(zip(nombres, [0] * len(nombres)))
-
-# Puedes imprimir el diccionario resultante
-print(diccionario_nombres)
-
