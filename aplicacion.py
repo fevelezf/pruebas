b95037aa107f8aca8672c99614d139ff3f4bd02d
Sebastian Aguinaga Velásquez <saguinagavelasquez@gmail.com>
2023-10-28 15:49:12 -0500

    Con el selectbox

--- aplicacion.py
+++ aplicacion.py
@@ -327,7 +327,7 @@ if get_current_user() is not None:
             lista = df["fon_name"].to_list()
             selected_fon = st.selectbox("Elija el fondo al que desee acceder",
                         lista)
-            if st.button("Mostrar"):
+            if selected_fon:
                 User = Query()
                 username = st.session_state.username
                 fon_data = db_us_fon_com.search(
